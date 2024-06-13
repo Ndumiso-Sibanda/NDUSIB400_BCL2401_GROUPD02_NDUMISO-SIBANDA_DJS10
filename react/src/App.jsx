3b38ce184c87d9aebed3cda6691f71f7709d8cf9
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-06-13 16:36:42 +0200

    Add state variables for posts and error

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,3 +1,8 @@
 import React from "react"
 import './App.css'
 import imgUrl from './assets/images/error-message.png'
+
+function App() {
+  const [posts, setPosts] = React.useState([])
+  const [error, setError] = React.useState(null);
+}
